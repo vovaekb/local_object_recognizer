a1711aae4462fbc84737958078592dda211e8b2e
Vladimir Privalov <vovaprivalov@gmail.com>
2024-01-04 13:05:07 +0500

    Refactor global variables with constexpr

--- local_object_recognizer/include/local_object_recognizer/common.h
+++ local_object_recognizer/include/local_object_recognizer/common.h
@@ -4,40 +4,39 @@
 #include <string>
 #include "local_object_recognizer/recognizer.h"
 
-extern bool perform_verification;
-extern bool visualize;
-extern bool use_hough;
-extern bool use_hv;
+constexpr bool perform_verification;
+constexpr bool visualize;
+constexpr bool use_hough;
+constexpr bool use_hv;
 
-extern bool perform_ransac;
+constexpr bool perform_ransac;
 // Algorithm params
-extern float descr_rad;
-extern double rf_radius;
-extern double cg_size;
-extern double cg_threshold;
+constexpr float descr_rad;
+constexpr double rf_radius;
+constexpr double cg_size;
+constexpr double cg_threshold;
 
-extern int sac_max_iters;
-extern const int icp_max_iter;
-extern float sac_inlier_thresh;
-extern float hv_inlier_thresh;
-
+constexpr int sac_max_iters;
+constexpr const int icp_max_iter;
+constexpr float sac_inlier_thresh;
+constexpr float hv_inlier_thresh;
 
 // Training scene and models paths
-extern std::string scene_pcd_file;
-extern std::string scene_name;
-extern std::string model_name;
-extern std::string models_dir;
-extern std::string model_descr_dir;
-extern std::string base_descr_dir;
-extern std::string gt_files_dir;
-extern string gt_file_path;
+constexpr std::string scene_pcd_file;
+constexpr std::string scene_name;
+constexpr std::string model_name;
+constexpr std::string models_dir;
+constexpr std::string model_descr_dir;
+constexpr std::string base_descr_dir;
+constexpr std::string gt_files_dir;
+constexpr string gt_file_path;
 
-extern const float icp_corr_distance;
-extern const float icp_fitness_eps;
-extern const float hv_clutter_reg;
-extern const float hv_rad_clutter;
-extern const float hv_regularizer;
+constexpr const float icp_corr_distance;
+constexpr const float icp_fitness_eps;
+constexpr const float hv_clutter_reg;
+constexpr const float hv_rad_clutter;
+constexpr const float hv_regularizer;
 
-extern Recognizer recognizer;
+constexpr Recognizer recognizer;
 
 #endif // COMMON_H
